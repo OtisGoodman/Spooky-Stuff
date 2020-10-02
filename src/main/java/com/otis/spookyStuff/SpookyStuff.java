115d82e7bd937ad45f2e3713fef68d7730ee20a5
OtisGoodman <hubbert.pumpkin@gmail.com>
2020-10-01 21:46:18 -0500

    Added mod files!

---
+++ src/main/java/com/otis/spookyStuff/SpookyStuff.java
@@ -0,0 +1,18 @@
+package com.otis.spookyStuff;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(SpookyStuff.MOD_ID)
+public class SpookyStuff {
+    
+    public static final String MOD_ID = "spookyStuff";
+    
+    public static final String MOD_NAME = "Spooky Stufff";
+    
+    public static final Logger LOG = LogManager.getLogger(MOD_NAME);
+    
+    
+}
